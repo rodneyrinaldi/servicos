1e57cc5c2e5625908f85d97015736c60436d956a
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2025-11-10 17:47:29 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from 'next';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: 'Task Pilot - Agendamento de Tarefas Jurídicas',
-  description: 'Acesso instantâneo a movimentações judiciais e agendamento automático no seu calendário.',
+  title: 'Serviços Advocatícios',
+  description: 'Automação para Escritórios de Advocacia.',
 };
 
 export default function RootLayout(
